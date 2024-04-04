4c7b6dea30bbf668253f5807a46bb88607c0dee3
victor <wahinyavictor818@gmail.com>
2024-04-04 12:53:54 +0300

    feat:index.js -m

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+//function addingEventListener() {
+    //const input = document.getElementById('button');
+    //const clickHandler =function () {
+      //  alert('i was clicked');
+    //};
+    //input.addEventListener('click',clickHandler);
+//}
 function addingEventListener() {
-}
+    const input = document.getElementById("button");
+    input.addEventListener("click", function () {
+      alert("I was clicked!");
+    });
+  }
